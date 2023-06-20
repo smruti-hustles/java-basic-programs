7717cabfb2ab1f87bd777734ee6f5d2716348d97
smruti rekha pradhan <136621177+smruti-hustles@users.noreply.github.com>
2023-06-20 12:44:57 +0530

    Add files via upload

---
+++ SupExample.java
@@ -0,0 +1,24 @@
+class A{
+  A(){
+System.out.println("hello there");
+}
+}
+class B extends A{
+   B(){
+   super();
+System.out.print("whatsapp");
+}
+}
+class SupExample{
+public static void main(String args[]){
+ B obj=new B();
+}
+}
+
+
+
+
+
+
+
+
