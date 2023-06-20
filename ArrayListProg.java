c30d510df1a1833e39318c014052b9176b81a374
smruti rekha pradhan <136621177+smruti-hustles@users.noreply.github.com>
2023-06-20 12:27:30 +0530

    Add files via upload

---
+++ ArrayListProg.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class ArrayListProg
+{
+	public static void main(String[] args) {
+	    ArrayList<String> l=new ArrayList<String>();
+	    l.add("cse");
+	    l.add("it");
+	    l.add("ece");
+	    System.out.println("ArrayList is");
+           for(String branches:l)
+        {
+            System.out.println(branches);
+          }
+	}
+}
