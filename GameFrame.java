58b8673c8c70b6ba619a98a5b21be1825cdcdd37
smruti rekha pradhan <136621177+smruti-hustles@users.noreply.github.com>
2023-06-20 12:37:52 +0530

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.*;
+import javax.swing.*;
+import java.util.Random;
+
+public class GameFrame extends JFrame{
+    GameFrame(){
+
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
+}
